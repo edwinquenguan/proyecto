c73ffcf1ad8d6a9f8ca920a0dd0af36d6945c584
edwinquenguan <edwinquenguan14@gmail.com>
2025-09-06 09:26:17 -0500

    trabajo en clase

---
+++ cliente/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Dashboard from './components/pages/Dashboard.js';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+   <div className="App">
+    <Dashboard />
+   </div>
+  
+  );
+}
+
+export default App;
---
+++ cliente/src/components/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+        <p>Esta es la página de inicio del Trainlinker.</p>
+        <img src="logo tranclinker.jpeg" alt="Logo de la aplicaciónlogo tranclinker.jpeg" />
+      
+    </div>
+  );
+}
+export default Dashboard;
